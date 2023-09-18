178f2eae6158c618e201843385841bb00c442e6c
jewelrana77 <mdjewelrana.cse.pust@gmail.com>
2023-09-18 11:04:57 +0600

    x2-2x-5 function bisectional method

--- Bisectional_method.cpp
+++ Bisectional_method.cpp
@@ -1,8 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define e 0.0001
+#define f(x) pow(x,2)-2*x-5
 int main(){
-int n;
-cin>>n;
-cout<<n*n<<"\n";
-   return 0; 
+float x0,x1,x2;
+float f0,f1,f2;
+int i=0;
+
+cout<<"enter the x0 and x1:"<<"\n";
+cin>>x0>>x1;
+do{
+f0=f(x0);
+f1=f(x1);
+x2=(x0+x1)/2;
+f2=f(x2);
+if(f0*f1<0){
+   x1=x2;
+}
+else{
+   x0=x2;
+}
+i++;
+cout<<"number of iteration:"<<i<<" "<<"root value:"<<x2<<" "<<"value of function:"<<f2<<"\n";
+}
+while(fabs(f2)>e and i<11);
+return 0; 
 }
